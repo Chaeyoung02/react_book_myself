46037001b3807825dc8617d35716574f2d1a98b1
Chaeyoung02 <chaedud02@naver.com>
2023-04-02 17:05:11 +0900

    ch.04

--- src/pages/ch.04/HigherOrderRadioInputTest.tsx
+++ src/pages/ch.04/HigherOrderRadioInputTest.tsx
@@ -1,10 +1,35 @@
-//Title 컴포넌트를 사용하는 기본 파일
-import { Title } from '../../components'
-export default function CopyMe() {
+//고차 함수로 라디오 버튼 선택 로직 구현
+import { useMemo, useCallback, useState } from 'react'
+import * as D from '../../data'
+import { Title, Subtitle } from '../../components'
+
+export default function HigherOrderRadioInputTest() {
+    const jobTitles = useMemo(() => D.makeArray(4).map(D.randomJobTitle), [])
+    const [selectedIndex, setSelectedIndex] = useState<number>(0)
+    const onChange = useCallback(
+        (index: number) => () => setSelectedIndex(notUsed => index),
+        []
+    )
+    const radioInputs = useMemo(
+        () => 
+            jobTitles.map((value, index) => (
+                <label key ={index} className="flex justify-start cursor-pointer label">
+                    <input type="radio" name="jobs" className="mr-4 radio radio-primary" checked={index === selectedIndex} onChange={onChange(index)}/>
+                    <span className="label-text">{value}</span>
+                </label>
+            )),
+                [jobTitles, selectedIndex, onChange]
+    )
     return (
         <section className="mt-4">
-            <h2 className="font-bold text-5xl text-center">CopyMe</h2>
-            <div className="mt-4"></div>
+            <Title>RadioInputTest</Title>
+            <div className="flex flex-col justify-center mt-4">
+                <Subtitle>What is your job?</Subtitle>
+                <Subtitle className="mt-4">Selected Job: {jobTitles[selectedIndex]} </Subtitle>
+                <div className="flex justify-center p-4 mt-4">
+                    <div className="flex flex-col mt-4">{radioInputs}</div>
+                </div>
+            </div>           
         </section>
     )
 }
